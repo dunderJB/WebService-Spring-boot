2484f1a37767e5be52caf8c24959441e7af1fe4f
Jordan Boaventura <j-boaventura@outlook.com>
2020-04-08 01:53:50 -0300

    Adicionando base de testes nativa

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -2,10 +2,20 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
+	
 	public String name;
 	public String email;
 	public String phone;
